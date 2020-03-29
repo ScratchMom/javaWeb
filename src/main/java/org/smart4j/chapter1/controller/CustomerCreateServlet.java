74a1960c2c1da56553b6fa3777ffc0565fd9595e
laowang666 <123456789@qq.com>
2020-03-29 14:41:32 +0800

    完善视图层

--- src/main/java/org/smart4j/chapter1/controller/CustomerCreateServlet.java
+++ src/main/java/org/smart4j/chapter1/controller/CustomerCreateServlet.java
@@ -1,11 +1,15 @@
 package org.smart4j.chapter1.controller;
 
+import org.smart4j.chapter1.model.Customer;
+import org.smart4j.chapter1.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author laowang
@@ -15,8 +19,18 @@ import java.io.IOException;
 @WebServlet("/customer_create")
 public class CustomerCreateServlet extends HttpServlet {
 
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
         super.doGet(req, resp);
     }
 
